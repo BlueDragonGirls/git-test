ae2d9492a0ff6acd9bccb6bed0f62b656fdd524f
zjc <zhangjiachenglv@163.com>
2018-09-23 18:05:56 +0800

    第四次提交

--- src/main/java/test/Test1.java
+++ src/main/java/test/Test1.java
@@ -4,4 +4,9 @@ package test;
  * Created by Administrator on 2018/9/23.
  */
 public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("第一次");
+        System.out.println("第二次");
+    }
+
 }
